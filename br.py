00d5ae6dd51bc576850ef66ea7f86e16dc3fd8e0
saurabh <shraddhasaini99@gmail.com>
2018-03-27 15:35:40 +0530

    [2001]

--- br.py
+++ br.py
@@ -1,16 +1,2 @@
 
 print "Shraddha188888"
-print "shraddha188"
-a= 16
-b = 18
-
-aa = "199"
-
-print a, b, aa
-print a+b
-print aa + str(b)
-print int(aa) + b
-c = aa + str(b)
-print c
-d = str(int(aa + str(b)) + int(aa)) + str(b)
-print int(d)+int(d)*int(d)^2*3/4
